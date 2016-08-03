46eb9a4d278e498147c26a55a962c7047881d7de
AfroMonkey <moisalejandro@gmail.com>
2016-08-03 13:58:39 -0500

    [IMP] depends

--- __openerp__.py
+++ __openerp__.py
@@ -3,9 +3,9 @@
     'version': '1.0',
     'author': 'Humanytek',
     'website': 'http://humanytek.com',
-    'depends': ['stock'],
+    'depends': ['stock', 'purchase_contract_type'],
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         # 'security/trucks_reception_access_rules.xml',
         'views/trucks_reception.xml',
     ]
